0940a8ed951bdc1ad172c60bcb75016987e74326
cmurph56 <cammurphy195@gmail.com>
2020-05-12 22:02:31 -0500

    implemented a trending module

--- components/Trending.vue
+++ components/Trending.vue
@@ -2,7 +2,8 @@
   <section>
     <h3> Trending </h3>
       <div v-for="hashTag in trendingHashtags" :key="hashTag">
-          {{hashTag}}
+          <span class="headline">{{hashTag.content}}</span> 
+          {{hashTag.count}} are tweeting about this
       </div>
   </section>
 
@@ -27,12 +28,30 @@ export default {
       var tweets = this.$store.state.Tweets.posts
       for(var i = 0; i < tweets.length; i++){
         for(var j = 0; j < tweets[i]['hashTags'].length; j++){
-          hashtags.push(tweets[i]['hashTags'][j])
-        }
+          var hashTagContent = tweets[i]['hashTags'][j].toLowerCase().replace(/(\r\n|\n|\r)/gm, "");
+          if(!hashtags.find( x => x.content == hashTagContent)){
+            hashtags.push(
+              { 
+                content: hashTagContent, 
+                count: 1
+              });
+            }
+            else {
+              let found = hashtags.find(element => element.content == hashTagContent);
+              found.count = found.count + 1;
+            }
+          }
       }
+      hashtags.sort((a,b) => (a.count > b.count) ? -1 : 1 )
       return hashtags
     }  
   }
 }
 
 </script>
+
+<style>
+.headline{
+  font-weight: bold;
+}
+</style>
