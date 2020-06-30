3d447f0cb800ad3b3a6e4482c2ffcffb7dcc21cd
cmurph56 <cammurphy195@gmail.com>
2020-06-29 19:31:31 -0500

    just fixing some stuff

--- server/index.js
+++ server/index.js
@@ -5,12 +5,14 @@ const app = express()
 const tweetsRouter = require('./tweet/tweet.router')
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
+
+const tweetModel = require('./tweet/tweets.model.js')
+
 
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
 config.dev = process.env.NODE_ENV !== 'production'
-
+  
 async function start () {
   const nuxt = new Nuxt(config)
   const { host, port } = nuxt.options.server
@@ -21,22 +23,10 @@ async function start () {
   }
   dotenv.config()
 
-  app.use(bodyParser.urlencoded({extended: false}))
+  app.use(express.json())
 
-  var Tweet = require('./tweet/tweets.model')
-
-  app.post('/api/tweets/', (req, res) => {
-    mongoose.connect(process.env.MONGO_URI, {useUnifiedTopology: true, useNewUrlParser: true})
-    const connection = mongoose.connection
-    connection.once("open", function (){
-      console.log("successful connection established")
-      const tweet = new Tweet(req.body)
-      connection.db.collection('tweets').insertOne(
-        tweet
-      )
-    })
-  })
-
+  app.use('/api/tweets/', tweetsRouter)
+  
   // express work 
   app.use(express.json());
   // Give nuxt middleware to express
--- server/tweet/tweet.router.js
+++ server/tweet/tweet.router.js
@@ -2,20 +2,25 @@ const express = require('express')
 const tweetsRouter = express.Router();
 const tweetModel = require('./tweets.model.js')
 const connect = require('../utils/db')
+const mongoose = require('mongoose')
+
 // Twitter Post Method
 tweetsRouter.route('/')
   .post((req, res) => {
 
-    var db = connect
-    const tweet = new tweetModel(req.body)
-    console.log(tweet)
-    try {
-      const newTweet =  db.tweets.insertOne(tweet)
-      res.status(201).json(newTweet);
-    } catch (err) {
-      console.log(err.message)
-      res.status(400).json({ message: err.message })
-    }
+    mongoose.connect(process.env.MONGO_URI, {useUnifiedTopology: true, useNewUrlParser: true})
+    const connection = mongoose.connection
+    connection.once("open", function(){
+      const newTweet = new tweetModel(req.body)
+      console.log(newTweet)
+      try {
+        connection.db.collection('tweets').insertOne(newTweet)
+        res.status(201).json(newTweet);
+      } catch (err) {
+        console.log(err.message)
+        res.status(400).json({ message: err.message })
+      }
+    })
   })
 
 tweetsRouter.route('/:id')
